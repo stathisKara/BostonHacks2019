00e5b031bebb65d645650dc4eabeb585f301ac00
ThanasisFilippidis <sdi1400215@di.uoa.gr>
2019-11-16 15:58:29 -0500

    Created starting models and basic admin functionality.

--- remempill/admin.py
+++ remempill/admin.py
@@ -1,3 +1,118 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
-# Register your models here.
+from .models import MyUser, GrandParent, Pill
+
+
+class UserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = MyUser
+        fields = ('email', 'username')
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+    password = ReadOnlyPasswordHashField(label="Password",
+                                         help_text=("Raw passwords are not stored, so there is no way to see "
+                                                    "this user's password, but you can change the password "
+                                                    "using <a href=\"../password/\">this form</a>."))
+
+    class Meta:
+        model = MyUser
+        fields = ('email', 'username', 'password', 'is_active', 'is_admin')
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
+
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('id', 'email', 'username', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Personal info', {'fields': ()}),
+        ('Permissions', {'fields': ('is_admin', 'is_active')}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2')}
+         ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+    # def save_model(self, request, obj, form, change):
+    #     # Override this to set the password to the value in the field if it's
+    #     # changed.
+    #     if obj.pk:
+    #         orig_obj = models.MyUser.objects.get(pk=obj.pk)
+    #         if obj.password != orig_obj.password:
+    #             obj.set_password(obj.password)
+    #     else:
+    #         obj.set_password(obj.password)
+    #     obj.save()
+
+
+class GrandParentAdmin(admin.ModelAdmin):
+    fields = ['name', 'surname', 'user']
+    list_display = ['id', 'name', 'surname', 'user']
+
+
+class PillAdmin(admin.ModelAdmin):
+    fields = ['name', 'shape', 'color', 'size', 'remaining', 'owner']
+    list_display = ['id', 'name', 'shape', 'color', 'size', 'remaining', 'owner']
+
+
+# class PlaylistAdmin(admin.ModelAdmin):
+#     fields = ['name', 'songs_ids', 'local_id', 'tracks', 'user']
+#     list_display = ['id', 'name', 'local_id', 'user']
+
+
+# Now register the new UserAdmin...
+admin.site.register(MyUser, UserAdmin)
+admin.site.register(GrandParent, GrandParentAdmin)
+admin.site.register(Pill, PillAdmin)
+# ... and, since we're not using Django's built-in permissions,
+# unregister the Group model from admin.
+admin.site.unregister(Group)
--- remempill/models.py
+++ remempill/models.py
@@ -1,3 +1,117 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
+from django.utils import timezone
 
-# Create your models here.
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            # creation_date=creation_date,
+            username=username,
+            # premium_ending_date=premium_ending_date,
+            is_premium=False,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            username,
+            email,
+            password=password,
+            # creation_date=creation_date,
+            # premium_ending_date=premium_ending_date,
+            # is_premium=True,
+            # list_of_tracks="",
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class MyUser(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    # creation_date = models.DateField()
+    username = models.CharField(blank=True, null=True, max_length=150)
+    creation_date = models.DateTimeField(editable=False, default=timezone.now)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    EMAIL_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+
+    def get_id(self):
+        return self.id
+
+    # def update_total_playlists(self, number):
+    #     self.total_inserted_playlists_number = number
+    #     self.save(update_fields=["total_inserted_playlists_number"])
+    #
+    # def get_total_tracks(self):
+    #     return self.total_inserted_tracks_number
+
+
+class GrandParent(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='grandparent')
+
+    def __str__(self):
+        return self.name
+
+
+class Pill(models.Model):
+    name = models.CharField(max_length=30)
+    shape = models.CharField(max_length=15)
+    color = models.CharField(max_length=15)
+    size = models.CharField(max_length=10)
+    remaining = models.IntegerField()
+
+    owner = models.ForeignKey(GrandParent, on_delete=models.CASCADE, related_name="pill")
+
+    def __str__(self):
+        return self.name
